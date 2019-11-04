0f5e9c79150dac7a093445f0519657d44a04f5bd
Jonathan Kablan <jonathan.kablan@gmail.com>
2019-11-04 16:13:16 +0100

    first config init

---
+++ FastEntityBundle.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Jonathankablan\Bundle\FastEntityBundle;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Jonathan Kablan <jonathan.kablan@gmail.com>
+ */
+class FastEntityBundle extends Bundle
+{
+    public const VERSION = '2.3.3-DEV';
+
+    public function boot()
+    {
+        // noop
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+
+    public function registerCommands(Application $application)
+    {
+        // noop
+    }
+}
