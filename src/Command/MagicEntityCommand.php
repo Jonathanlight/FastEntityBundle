d3a6f5988a7b0665f0a9b45b1cb8671ddd8ff3ba
Jonathan <jonathan.kablan@gmail.com>
2020-02-16 22:29:41 +0100

    create Maker template init

--- src/Command/MagicEntityCommand.php
+++ src/Command/MagicEntityCommand.php
@@ -2,16 +2,8 @@
 
 namespace Jonathankablan\Bundle\FastEntityBundle\Command;
 
-use Jonathankablan\Bundle\FastEntityBundle\Configuration\SettingManager;
-use Symfony\Bundle\MakerBundle\ConsoleStyle;
-use Symfony\Bundle\MakerBundle\DependencyBuilder;
-use Symfony\Bundle\MakerBundle\Generator;
-use Symfony\Bundle\MakerBundle\InputConfiguration;
-use Symfony\Bundle\MakerBundle\Str;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -29,25 +21,11 @@ class MagicEntityCommand extends Command
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
-     * @param Generator $generator
      * @return int
      */
-    protected function execute(InputInterface $input, OutputInterface $output, Generator $generator): int
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-
-        $setting = new SettingManager();
-        dump($setting->readYamlConfig());
-
-        $generator->generateClass(
-            $commandClassNameDetails->getFullName(),
-            'command/Command.tpl.php',
-            [
-                'command_name' => $commandName,
-            ]
-        );
-
-        $generator->writeChanges();
 
         $io->success('You have a new command!');
 
---
+++ src/Maker/MakeMagicEntity.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
+ * This file is part of the Symfony MakerBundle package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\MakerBundle\Maker;
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\Common\Annotations\Annotation;
+use Doctrine\Common\Inflector\Inflector;
+use Jonathankablan\Bundle\FastEntityBundle\Configuration\SettingManager;
+use Symfony\Bundle\MakerBundle\ConsoleStyle;
+use Symfony\Bundle\MakerBundle\DependencyBuilder;
+use Symfony\Bundle\MakerBundle\Doctrine\DoctrineHelper;
+use Symfony\Bundle\MakerBundle\FileManager;
+use Symfony\Bundle\MakerBundle\Generator;
+use Symfony\Bundle\MakerBundle\InputConfiguration;
+use Symfony\Bundle\MakerBundle\Str;
+use Symfony\Bundle\MakerBundle\Validator;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+final class MakeMagicEntity extends AbstractMaker
+{
+    /**
+     * @var SettingManager
+     */
+    protected $setting;
+
+    /**
+     * @var FileManager
+     */
+    private $fileManager;
+
+    /**
+     * @var DoctrineHelper
+     */
+    private $doctrineHelper;
+
+    /**
+     * @var Generator
+     */
+    private $generator;
+
+    /**
+     * @param SettingManager $setting
+     * @param FileManager $fileManager
+     * @param DoctrineHelper $doctrineHelper
+     * @param string $projectDirectory
+     * @param Generator|null $generator
+     */
+    public function __construct(
+        SettingManager $setting,
+        FileManager $fileManager,
+        DoctrineHelper $doctrineHelper,
+        string $projectDirectory,
+        Generator $generator = null
+    )
+    {
+        $this->setting = $setting;
+        $this->fileManager = $fileManager;
+        $this->doctrineHelper = $doctrineHelper;
+        // $projectDirectory is unused, argument kept for BC
+
+        if (null === $generator) {
+            @trigger_error(sprintf('Passing a "%s" instance as 4th argument is mandatory since version 1.5.', Generator::class), E_USER_DEPRECATED);
+            $this->generator = new Generator($fileManager, 'App\\');
+        } else {
+            $this->generator = $generator;
+        }
+    }
+
+    public static function getCommandName(): string
+    {
+        return 'magic:fast:entity';
+    }
+
+    public function configureCommand(Command $command, InputConfiguration $inputConf)
+    {
+        $command
+            ->setDescription('Generate structure entity')
+            ->setHelp(file_get_contents(__DIR__.'/../Resources/help/MakeEntity.txt'))
+        ;
+    }
+
+    public function generate(InputInterface $input, ConsoleStyle $io, Generator $generator)
+    {
+        $entityClassDetails = $generator->createClassNameDetails(
+            'Student',
+            'Entity\\',
+            'Entity'
+        );
+
+        $entityDoctrineDetails = $this->doctrineHelper->createDoctrineDetails($entityClassDetails->getFullName());
+
+        $repositoryVars = [];
+
+        if (null !== $entityDoctrineDetails->getRepositoryClass()) {
+            $repositoryClassDetails = $generator->createClassNameDetails(
+                '\\'.$entityDoctrineDetails->getRepositoryClass(),
+                'Repository\\',
+                'Repository'
+            );
+
+            $repositoryVars = [
+                'repository_full_class_name' => $repositoryClassDetails->getFullName(),
+                'repository_class_name' => $repositoryClassDetails->getShortName(),
+                'repository_var' => lcfirst(Inflector::singularize($repositoryClassDetails->getShortName())),
+            ];
+        }
+
+        dump($this->setting->readYamlConfig());
+
+        $controllerPath = $generator->generateController(
+            $entityClassDetails->getFullName(),
+            'doctrine/Entity.tpl.php',
+            [
+                'route_path' => Str::asRoutePath($entityClassDetails->getRelativeNameWithoutSuffix()),
+                'route_name' => Str::asRouteName($entityClassDetails->getRelativeNameWithoutSuffix()),
+            ]
+        );
+
+        $generator->writeChanges();
+
+        $this->writeSuccessMessage($io);
+        $io->text('Generate ...');
+    }
+
+    public function configureDependencies(DependencyBuilder $dependencies)
+    {
+        $dependencies->addClassDependency(
+            DoctrineBundle::class,
+            'orm-pack'
+        );
+
+        $dependencies->addClassDependency(
+            Annotation::class,
+            'doctrine/annotations'
+        );
+    }
+}
